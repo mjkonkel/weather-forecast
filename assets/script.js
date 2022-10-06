d73bf9739b695b723d9914c3ce737f0c62ae5e14
mjkonkel <mitchell.konkel@gmail.com>
2022-10-05 22:13:48 -0500

    created more detail page structure

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,19 @@
 var todayDate = moment().format('L');
 $('#currentDay').text(todayDate);
+
+var dayOne = moment().add(1, 'days');
+$('#dayOne').text(dayOne.format('L'));
+
+var dayTwo = moment().add(2, 'days');
+$('#dayTwo').text(dayTwo.format('L'));
+
+
+
+// var days = [1,2,3,4,5]
+
+// for (let i = 0; i < days.length; i++){
+//     var dates = moment().add(days[i], 'days')
+//     console.log(dates)
+//     // $('#dayOne').text(days[i].format('L'));
+
+// }
