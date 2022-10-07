01f3ba0ace43481afc59515f1eb713173eaa8895
mjkonkel <mitchell.konkel@gmail.com>
2022-10-06 21:49:06 -0500

    added api and logged data

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,19 @@ var dayTwo = moment().add(2, 'days');
 $('#dayTwo').text(dayTwo.format('L'));
 
 
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=52.52&lon=13.40&appid=23370ae6f515fc79e6570bdda3897515'
 
-// var days = [1,2,3,4,5]
+fetch(requestUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+    console.log(data);
+})
 
-// for (let i = 0; i < days.length; i++){
-//     var dates = moment().add(days[i], 'days')
-//     console.log(dates)
-//     // $('#dayOne').text(days[i].format('L'));
 
-// }
+
+
+   
+
+      
